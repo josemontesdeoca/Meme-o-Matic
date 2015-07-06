01f4cb9b3a0dc0171b24e78103192cb802029c97
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2015-07-05 20:12:19 -0700

    Binding UI elements and passing top/bottom text to the meme

--- app/src/main/java/com/joseonline/android/memgen/CreateMemeActivity.java
+++ app/src/main/java/com/joseonline/android/memgen/CreateMemeActivity.java
@@ -2,16 +2,67 @@ package com.joseonline.android.memgen;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class CreateMemeActivity extends ActionBarActivity {
+    private EditText etTopText;
+    private EditText etBottomText;
+    private TextView tvTopText;
+    private TextView tvBottomText;
+    private ViewGroup rlMeme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_meme);
+
+        // Bind UI elements
+        etTopText = (EditText) findViewById(R.id.etTopText);
+        etBottomText = (EditText) findViewById(R.id.etBottomText);
+        tvTopText = (TextView) findViewById(R.id.tvTopText);
+        tvBottomText = (TextView) findViewById(R.id.tvBottomText);
+        rlMeme = (ViewGroup) findViewById(R.id.rlMeme);
+
+        etTopText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                tvTopText.setText(editable.toString());
+            }
+        });
+
+        etBottomText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                tvBottomText.setText(editable.toString());
+            }
+        });
     }
 
 
